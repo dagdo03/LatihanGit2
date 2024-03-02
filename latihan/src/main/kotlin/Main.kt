830c67fdefd7a10f5eed28b82be8e50a4886f20f
RaihanRafi44 <raihanrrizqullah@gmail.com>
2024-03-02 08:25:05 +0700

    Finishing circle feature

--- latihan/src/main/kotlin/Main.kt
+++ latihan/src/main/kotlin/Main.kt
@@ -1,3 +1,29 @@
-fun main() {
-    println("Hello World!")
+import kotlin.math.PI
+
+class Circle(private val radius: Double) {
+
+    fun getRadius(): Double {
+        return radius
+    }
+
+    fun areaCircleCalculate(): Double { //method to calculate area of circle
+        return PI * radius * radius
+    }
+
+    fun circumferenceCircleCalculate(): Double { //method to calculate circumference of circle
+        return 2 * PI * radius
+    }
+
+    fun printHeaderCircle(){ //method to print header of circle
+        println("Perhitungan Lingkaran")
+        println("-------------------------------------")
+    }
+}
+fun main(args: Array<String>) {
+    val radius = 42.0
+    val circle = Circle(radius)
+    circle.printHeaderCircle()
+    println("Jari-jari lingkaran adalah ${circle.getRadius()}")
+    println("Luas lingkaran dengan jari-jari yaitu $radius adalah ${circle.areaCircleCalculate()}")
+    println("Keliling lingkaran dengan jari-jari yaitu $radius adalah ${circle.circumferenceCircleCalculate()}")
 }
