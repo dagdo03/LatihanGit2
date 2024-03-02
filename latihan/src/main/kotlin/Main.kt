0fdc1612607100e7eddde7940c0177c7e70bf5e5
RaihanRafi44 <raihanrrizqullah@gmail.com>
2024-03-02 08:40:41 +0700

    Finishing square perimeter feature

--- latihan/src/main/kotlin/Main.kt
+++ latihan/src/main/kotlin/Main.kt
@@ -28,19 +28,37 @@ class Triangle(){
     }
 }
 
-class Square(){
-    fun wideSquare(long: Double){
-        println("Wide is : ${long * long}")
+class Square(private val side: Double){
+    fun wideSquare(){
+        println("Wide is : ${side * side}")
+    }
+    fun getSide(): Double {
+        return side
+    }
+
+    fun perimeterSquareCalculate(): Double { //method to calculate perimeter of square
+        return 4 * side
+    }
+
+    fun printHeaderSquare(){ //method to print header of square
+        println("Perhitungan Persegi")
+        println("-------------------------------------")
     }
 }
 fun main(args: Array<String>) {
     val radius = 42.0
     val circle = Circle(radius)
+    val side = 20.0
+    val square = Square(side)
     circle.printHeaderCircle()
     println("Jari-jari lingkaran adalah ${circle.getRadius()}")
     println("Luas lingkaran dengan jari-jari yaitu $radius adalah ${circle.areaCircleCalculate()}")
     println("Keliling lingkaran dengan jari-jari yaitu $radius adalah ${circle.circumferenceCircleCalculate()}")
     Triangle().circumferenceTriangle(3.0, 4.0, 5.0)
     Triangle().wideTriangle(4.0, 3.0)
-    Square().wideSquare(5.0)
+    Square(side).wideSquare()
+    square.printHeaderSquare()
+    println("Panjang sisi persegi adalah ${square.getSide()}")
+    println("Keliling persegi dengan panjang sisi yaitu $side adalah ${square.perimeterSquareCalculate()}")
+
 }
